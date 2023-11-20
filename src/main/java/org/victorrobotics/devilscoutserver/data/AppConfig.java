142628850f4e7aabfca4505abfba5afd74a9c973
MathNerd28 <xander.bhalla@gmail.com>
2023-11-19 22:08:19 -0500

    Remove unused AppConfig

--- src/main/java/org/victorrobotics/devilscoutserver/data/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.victorrobotics.devilscoutserver.data;
-
-import io.javalin.openapi.OpenApiExample;
-import io.javalin.openapi.OpenApiRequired;
-
-public record AppConfig(@OpenApiRequired @OpenApiExample("1559") int team,
-                        @OpenApiRequired @OpenApiExample("2023nyrr") String eventKey) {
-
-}
