b4da4d0b468206dc30079d7301f4335c43558c2b
MathNerd28 <xander.bhalla@gmail.com>
2023-11-19 21:52:57 -0500

    Remove unused server status

--- src/main/java/org/victorrobotics/devilscoutserver/data/ServerStatus.java
+++
@@ -1,6 +0,0 @@
-package org.victorrobotics.devilscoutserver.data;
-
-import io.javalin.openapi.OpenApiExample;
-import io.javalin.openapi.OpenApiRequired;
-
-public record ServerStatus(@OpenApiRequired @OpenApiExample("okay") String status) {}
