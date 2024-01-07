7a78b872bb7e89786d6cdba11e6e22a7fddaa8c0
MathNerd28 <xander.bhalla@gmail.com>
2024-01-07 16:24:08 -0500

    Import proposed 2024 questions

--- src/main/java/org/victorrobotics/devilscoutserver/questions/QuestionDeserializer.java
+++ src/main/java/org/victorrobotics/devilscoutserver/questions/QuestionDeserializer.java
@@ -17,45 +17,44 @@ public class QuestionDeserializer extends StdDeserializer<Question> {
 
   @Override
   public Question deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
-    JsonNode node = parser.readValueAsTree();
+    JsonNode json = parser.readValueAsTree();
 
-    String prompt = node.get("prompt")
+    String prompt = json.get("prompt")
                         .textValue();
-    String key = node.get("key")
+    String key = json.get("key")
                      .textValue();
-    JsonNode config = node.get("config");
 
-    QuestionType type = QuestionType.valueOf(node.get("type")
+    QuestionType type = QuestionType.valueOf(json.get("type")
                                                  .asText());
     return switch (type) {
       case BOOLEAN -> new BooleanQuestion(prompt, key);
       case COUNTER -> new CounterQuestion(prompt, key);
+      case NUMBER -> {
+        JsonNode min = json.get("min");
+        JsonNode max = json.get("max");
+        yield new NumberQuestion(prompt, key, min == null ? null : min.intValue(),
+                                 max == null ? null : max.intValue());
+      }
+      case RANGE -> {
+        int min = json.get("min")
+                      .intValue();
+        int max = json.get("max")
+                      .intValue();
+        JsonNode increment = json.get("increment");
+        yield new RangeQuestion(prompt, key, min, max,
+                                increment == null ? 1 : increment.intValue());
+      }
       case MULTIPLE, SEQUENCE, SINGLE -> {
         List<String> options = new ArrayList<>();
-        config.get("options")
-              .elements()
-              .forEachRemaining(e -> options.add(e.textValue()));
+        json.get("options")
+            .elements()
+            .forEachRemaining(e -> options.add(e.textValue()));
         yield switch (type) {
           case MULTIPLE -> new MultipleChoiceQuestion(prompt, key, options);
           case SEQUENCE -> new SequenceQuestion(prompt, key, options);
           case SINGLE -> new SingleChoiceQuestion(prompt, key, options);
           default -> throw new IllegalStateException();
         };
-      }
-      case NUMBER -> {
-        JsonNode min = config.get("min");
-        JsonNode max = config.get("max");
-        yield new NumberQuestion(prompt, key, min == null ? null : min.intValue(),
-                                 max == null ? null : max.intValue());
-      }
-      case RANGE -> {
-        int min = config.get("min")
-                        .intValue();
-        int max = config.get("max")
-                        .intValue();
-        JsonNode increment = config.get("increment");
-        yield new RangeQuestion(prompt, key, min, max,
-                                increment == null ? 1 : increment.intValue());
       }
     };
   }
